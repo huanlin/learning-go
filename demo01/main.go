b555b84d1dff710b31827d04500f00c213625518
Michael Tsai <huanlin.tsai@gmail.com>
2024-08-26 23:40:19 +0800

    -

--- demo01/main.go
+++ demo01/main.go
@@ -1,3 +1,25 @@
+/*
+Demo01 is a simple program that prints "Hello, World!" to the console.
+It also demonstrates how to write tests for Go programs.
+
+(1) To build the program, navigate to the directory containing the main.go file and run:
+
+go build
+
+This will compile the program and create an executable file in the same directory.
+
+(2) To run the program, navigate to the directory containing the executable file and run:
+
+./demo01
+
+This will run the program and print "Hello, World!" to the console.
+
+(3) To run the tests, navigate to the directory containing the hello_test.go file and run:
+
+go test
+
+This will run the tests and print the results to the console.
+*/
 package main
 
 import "fmt"
