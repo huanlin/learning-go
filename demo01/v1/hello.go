5ca56dc94ee3941ed67b092ce935b68f77320e28
Michael Tsai <huanlin.tsai@gmail.com>
2024-08-28 23:21:26 +0800

    -

--- demo01/v1/hello.go
+++ demo01/v1/hello.go
@@ -4,6 +4,29 @@ import (
 	"fmt"
 )
 
+type Animal struct {
+	name string
+}
+
+func (a *Animal) speak() string {
+	a.name = "aa"
+	switch a.name {
+	case "cat":
+		return "meow"
+	case "dog":
+		return "woof"
+	default:
+		return "nondescript animal noise?"
+	}
+}
+
 func main() {
 	fmt.Println("Hello, World!")
+
+	a := Animal{
+		name: "cat",
+	}
+
+	fmt.Println(a.speak())
+	fmt.Println(a.name)
 }
