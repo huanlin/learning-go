54934cc12bb9156d457b456aa39d2661015dcc7a
Michael Tsai <huanlin.tsai@gmail.com>
2024-08-29 22:52:41 +0800

    udpadate

--- demo01/v1/hello.go
+++ demo01/v1/hello.go
@@ -23,10 +23,21 @@ func (a *Animal) speak() string {
 func main() {
 	fmt.Println("Hello, World!")
 
-	a := Animal{
-		name: "cat",
-	}
+	cat1 := new(Animal)
+	cat2 := &Animal{}
+	var cat3 *Animal = new(Animal)
 
-	fmt.Println(a.speak())
-	fmt.Println(a.name)
+	cat1.name = "cat"
+	cat2.name = "cat"
+
+	fmt.Printf("%T\n", cat1)
+	fmt.Printf("%T\n", cat2)
+	fmt.Printf("%T\n", cat3)
+
+	var x interface{} = 7
+
+	switch x.(type) {
+	case int:
+		fmt.Println("int")
+	}
 }
